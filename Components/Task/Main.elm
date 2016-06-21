9a00be99484f54ec4e7ea4fd74b5b0df2eb54a8f
Sergio Marin <higher.vnf@gmail.com>
2016-06-21 19:19:50 -0400

    Add double click action to edit form

--- Components/Task/Main.elm
+++ Components/Task/Main.elm
@@ -1,28 +1,35 @@
 module Components.Task.Main exposing (..)
-import Html exposing (li, text, div, input, button)
+import Html exposing (..)
 import Html.Events exposing (..)
 import Json.Decode as Json
-import Html.Attributes exposing (class, type', style, checked)
+import Html.Attributes exposing (..)
 import Components.Task.Styles as AppStyles
+import Components.Form.Styles as FormStyles
 
-import String
-
-type Actions = NoOp
+type Actions 
+  = NoOp
   | Toggle
   | Delete
+  | EditMessage String
+  | SetEdit
+  | RemoveEdit
 
 type alias Model =
   { description : String
   , id : Int
   , completed : Bool
+  , edit : Maybe Bool
   }
 
+init: Model
 init =
   { description = ""
   , id = 0
-  , completed = False
+  , completed = False  
+  , edit = Nothing
   }
 
+update : Actions -> Model -> Maybe Model
 update action model =
   case action of
     NoOp ->
@@ -32,50 +39,107 @@ update action model =
       Just
         { model
         | completed = not model.completed
+        , edit = Nothing
         }
 
     Delete ->
       Nothing
 
-toggleOnClick =
-  Toggle
+    SetEdit -> 
+      Just 
+        { model 
+        | edit = Just True
+        }
 
-deleteOnClick =
-  Delete
+    EditMessage description ->
+      Just 
+        { model
+        | description = description
+        }
 
+    RemoveEdit ->
+      Just 
+        { model
+        | edit = Nothing
+        }
+
+setStyle : Bool -> String
 setStyle completed =
   if completed == True then "ready" else "not-ready"
 
+checkButton : Bool -> Html Actions
 checkButton completed =
   input
     [ type' "checkbox"
     , checked completed
     , style AppStyles.check
-    , onClick toggleOnClick
+    , onClick Toggle
     ]
     []
 
+taskDescription : String -> Html Actions
 taskDescription description =
   div
-    [ style AppStyles.description ]
+    [ style AppStyles.description 
+    , onDoubleClick SetEdit 
+    ]
     [ text description ]
 
+deleteButton : Html Actions
 deleteButton =
   button
-    [ onClick deleteOnClick
+    [ onClick Delete
     , style AppStyles.delete
     ]
     [ text "x" ]
 
+
+tagger code =
+  if code == 13 then RemoveEdit else NoOp
+
+
+needByEdit : Maybe Bool -> Bool
+needByEdit edit =
+  case edit of
+    Nothing ->
+      False
+
+    Just bool ->
+      bool
+ 
+view: Model -> Html Actions
 view task =
-  li
-    [ class (setStyle task.completed) ]
-    [ div
-        [ class "todo-task"
-        , style AppStyles.general
-        ]
-        [ checkButton task.completed
-        , taskDescription task.description
-        , deleteButton
-        ]
-    ]
+  let 
+    toShow = 
+      if (needByEdit task.edit |> not) then 
+        div
+          [ class "todo-task"
+          , style AppStyles.general
+          ]
+          [ checkButton task.completed
+          , taskDescription task.description
+          , deleteButton
+          ]
+      else
+        div
+          [ style FormStyles.form ]
+          [ input
+                [ id "new-todo"
+                , style FormStyles.input
+                , placeholder "What needs to be done?"
+                , autofocus True
+                , value task.description
+                , name "newTodo"
+                , required True
+                , on "input" (Json.map EditMessage targetValue)
+                , on "keyup" (Json.map tagger keyCode)
+                ]
+                []
+            ]
+  in
+    li
+      [ class (setStyle task.completed) ]
+      [ toShow ]
+
+
+
