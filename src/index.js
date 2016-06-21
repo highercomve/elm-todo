1c774910042149f98c5819f09433139405f224d4
Sergio Marin <higher.vnf@gmail.com>
2016-06-21 19:30:06 -0400

    Catch exeption on changes on localstorage schema

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 // inject bundled Elm app into div#main
 
 var Elm = require( '../Todo.elm' );
-var storedState = localStorage.getItem('todo');
-var startingState = storedState ? JSON.parse(storedState) : null;
-var app = Elm.Todo.fullscreen(startingState);
-app.ports.setStorage.subscribe(function(state) {
-  localStorage.setItem('todo', JSON.stringify(state));
-});
 
-    
+function init() {
+  var startingState = storedState ? JSON.parse(storedState) : null;
+  var app = Elm.Todo.fullscreen(startingState);
+  app.ports.setStorage.subscribe(function(state) {
+    localStorage.setItem('todo', JSON.stringify(state));
+  });
+
+}
+try {
+  init()
+} catch (e) {
+  localStorage.clear()
+  init()
+}
+
+
